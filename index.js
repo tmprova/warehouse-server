91d13cc0210474490b48787263cf2bbe2f486947
tmprova <applocker81@gmail.com>
2022-10-23 13:33:59 +0600

    handle update

--- index.js
+++ index.js
@@ -25,19 +25,59 @@ async function run() {
   try {
     await client.connect();
     const collection = client.db("warehouseManage").collection("inventory");
-
+    // api data for home && inventory
     app.get("/inventory", async (req, res) => {
       const query = {};
       const cursor = collection.find(query);
       const storage = await cursor.toArray();
       res.send(storage);
     });
-
+    // get item by id
     app.get("/inventory/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const inventory = await collection.findOne(query);
       res.send(inventory);
+    });
+
+    // update stock
+    app.put("/itemAddToStock/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const data = req.body;
+      console.log(data);
+      const filter = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          Quatity: data.updatedQuantity,
+        },
+      };
+      const result = await collection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
+
+    // app.put("/itemAdd/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const data = req.body;
+    //   console.log("from quantity", data);
+    //   const filter = { _id: ObjectId(id) };
+    //   const option = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       quatity: data.quantity,
+    //     },
+    //   };
+    //   const result = await collection.updateOne(filter, updateDoc, option);
+    //   res.send(result);
+    // });
+
+    // to post new data
+    app.post("/inventory", async (req, res) => {
+      const newItem = req.body;
+      console.log(newItem);
+      const result = await collection.insertOne(newItem);
+      res.send(result);
     });
   } finally {
     // await client.close();
